55992b2e715791cdf53d3b097f0dba6b053ee6b3
Mohneesh S <mohneesh0@gmail.com>
2021-03-08 18:09:19 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -40,26 +40,29 @@ test_images = []
 test_images.append(st.file_uploader('File uploader'))
 predictions = []
 
-with st.spinner(text = "Wait a Minute"):
-    for image in test_images:
-        if image != None:
-            img = Image.open(image)
-            img = img.resize(size)
-            print(img)
-            img = np.expand_dims(img, axis=0)
-            print(img.shape)
-            predictions.extend(best_model.predict(img).argmax(axis = 1))
-            values = best_model.predict(img)[0]
-            st.text('Probability Values:' + str(values))
-            data = pd.DataFrame({'values':values})
-            st.bar_chart(data)
-            st.text("""
-            0 : Cassava Bacterial Blight (CBB)
-            1 : Cassava Mosaic Disease (CMD)
-            2 : Cassava Brown Streak Disease (CBSD)
-            3 : Cassava Green Mottle (CGM)
-            4 : Healthy
-            """)
-            st.success("Here are the Results")
+button = st.button("Click to find your Top 10!")
+
+if button:
+    with st.spinner(text = "Wait a Minute"):
+        for image in test_images:
+            if image != None:
+                img = Image.open(image)
+                img = img.resize(size)
+                print(img)
+                img = np.expand_dims(img, axis=0)
+                print(img.shape)
+                predictions.extend(best_model.predict(img).argmax(axis = 1))
+                values = best_model.predict(img)[0]
+                st.text('Probability Values:' + str(values))
+                data = pd.DataFrame({'values':values})
+                st.bar_chart(data)
+                st.text("""
+                0 : Cassava Bacterial Blight (CBB)
+                1 : Cassava Mosaic Disease (CMD)
+                2 : Cassava Brown Streak Disease (CBSD)
+                3 : Cassava Green Mottle (CGM)
+                4 : Healthy
+                """)
+                st.success("Here are the Results")
 
     
